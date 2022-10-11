0d603247f050d1d4090029b7d7f2e24ffdeb5644
Marc González Moratona <80526953+marcelinus99@users.noreply.github.com>
2022-10-11 18:30:24 +0200

    Update main.cpp

--- Vec3/main.cpp
+++ Vec3/main.cpp
@@ -3,38 +3,48 @@
 using namespace std;
 
 int main() {
-	Vec3<int> v(7,6,4);
-	Vec3<int> c(2,1,3);
-	cout << v.getX() << endl;
-	cout << v.getY() << endl;
-	cout << v.getZ() << endl;
+	Vec3<int> v(7, 6, 4);
+	Vec3<int> c(2, 1, 3);
+	cout << "Initial values:" << endl;
+	cout << "(" << v.getX() << ", " << v.getY() << ", " << v.getZ() << ")" << endl;
+	cout << "(" << c.getX() << ", " << c.getY() << ", " << c.getZ() << ")" << endl;
 
-	Vec3<int> r = v.dot_product(c);
-	cout << r.getX() << endl;
-	cout << r.getY() << endl;
-	cout << r.getZ() << endl;
+	Vec3<int> r2 = v.cross_product(c);
+	cout << endl << "Cross product:"  << "(" << r2.getX() << ", " << r2.getY() << ", " << r2.getZ() << ")" << endl;
+	cout << "Expected output: (14, 13, -5)" << endl;
 
-	Vec3<int> d(1,0,5);
-	Vec3<int> t(0,2,4);
-	float distance = d.distance_to(t);
-    cout << distance << endl;
+	cout << endl << "Setting new values:" << endl;
+	v.setX(2);
+	v.setY(7);
+	v.setZ(2);
+	c.setX(3);
+	c.setY(1);
+	c.setZ(5);
 
-	Vec3<float> n(5,6,10);
-	Vec3<float>r2 = n.Normalize();
-	cout << r2.getX() << endl;
-	cout << r2.getY() << endl;
-	cout << r2.getZ() << endl;
+	cout << "(" << v.getX() << ", " << v.getY() << ", " << v.getZ() << ")" << endl;
+	cout << "(" << c.getX() << ", " << c.getY() << ", " << c.getZ() << ")" << endl;
 
+	float r1 = v.dot_product(c);
+	cout << endl << "Dot product: " << r1 << endl;
+	cout << "Expected output: 23" << endl;
 
+	cout << endl << "Setting new values:" << endl;
+	v.setX(1);
+	v.setY(0);
+	v.setZ(5);
+	c.setX(0);
+	c.setY(2);
+	c.setZ(4);
+
+	cout << "(" << v.getX() << ", " << v.getY() << ", " << v.getZ() << ")" << endl;
+	cout << "(" << c.getX() << ", " << c.getY() << ", " << c.getZ() << ")" << endl;
+	
+	cout << endl << "Distance:" << endl;
+	float distance = v.distance_to(c);
+	cout << distance << endl;
+
+	cout << endl << "Normalisation:" << endl;
+	Vec3<float> n(5, 6, 10);
+	Vec3<float>r3 = n.Normalize();
+	cout << "(" << r3.getX() << ", " << r3.getY() << ", " << r3.getZ() << ")" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
