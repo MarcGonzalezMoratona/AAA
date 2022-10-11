6ad496f0699ba1769cc04d7f8f4db460e19047de
Marc González Moratona <80526953+marcelinus99@users.noreply.github.com>
2022-10-11 18:29:58 +0200

    Update Vec3.cpp

--- Vec3/Vec3.cpp
+++ Vec3/Vec3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 template <class T>
 Vec3<T>::Vec3() {
-    this->x = 0;
+	this->x = 0;
 	this->y = 0;
 	this->z = 0;
 }
@@ -41,22 +41,22 @@ T Vec3<T>::getZ() {
 }
 
 template <class T>
-T Vec3<T>::setX(T x) {
+void Vec3<T>::setX(T x) {
 	this->x = x;
 }
 
 template <class T>
-T Vec3<T>::setY(T y) {
+void Vec3<T>::setY(T y) {
 	this->y = y;
 }
 
 template <class T>
-T Vec3<T>::setZ(T z) {
+void Vec3<T>::setZ(T z) {
 	this->z = z;
 }
 
 template <class T>
-Vec3<T> Vec3<T>::Normalize() {	
+Vec3<T> Vec3<T>::Normalize() {
 	float length = sqrt(pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2));
 	return Vec3<T>(this->x / length, this->y / length, this->z / length);
 }
@@ -64,18 +64,18 @@ Vec3<T> Vec3<T>::Normalize() {
 template <class T>
 float Vec3<T>::distance_to(Vec3<T> v) {
 	Vec3<T> r(pow(this->x - v.x, 2), pow(this->y - v.y, 2), pow(this->z - v.z, 2));
-    return sqrt(r.getX() + r.getY() + r.getZ());
+	return sqrt(r.getX() + r.getY() + r.getZ());
 }
 
 template <class T>
-Vec3<T> Vec3<T>::dot_product(Vec3<T> v) {
-	return Vec3<T>((this->y * v.z - this->z * v.y), -(this->x * v.z - this->z * v.x), (this->x * v.x - this->y - v.x));
+T Vec3<T>::dot_product(Vec3<T> v) {
+	return (this->x * v.x + this->y * v.y + this->z * v.z);
 }
 
 
 template <class T>
-void Vec3<T>::cross_product(Vec3<T> v) {
-
+Vec3<T> Vec3<T>::cross_product(Vec3<T> v) {
+	return Vec3<T>((this->y * v.z - this->z * v.y), (this->x * v.z - this->z * v.x), (this->x * v.y - this->y * v.x));
 }
 
 template <class T>
